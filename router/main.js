25fce484f6e76d2100b1be19af6db0c706ff64b5
안준영/ajy92/LG CNS(트윈) <ajy92@lgchem.com>
2018-11-24 15:13:31 +0900

    mysql db connect

--- router/main.js
+++ router/main.js
@@ -2,7 +2,7 @@ module.exports =  function(app, fs)
 {
    app.get('/',function(req,res){
       res.render('index', {
-          title: "dhProject",
+          title: "MY HOMEPAGE",
           length: 5
       })
    });
