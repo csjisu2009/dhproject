95bc30c634487a82d1cbb01affe50f236864bec5
csjisu2009 <csjisu2009@gmail.com>
2018-11-24 13:15:00 +0900

    title changed as dhProject

--- router/main.js
+++ router/main.js
@@ -2,7 +2,7 @@ module.exports =  function(app, fs)
 {
    app.get('/',function(req,res){
       res.render('index', {
-          title: "MY HOMEPAGE",
+          title: "dhProject",
           length: 5
       })
    });
