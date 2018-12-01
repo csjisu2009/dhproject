12e72024026195e20a2a4297faa6ad3da89a48da
안준영/ajy92/LG CNS(트윈) <ajy92@lgchem.com>
2018-12-01 17:47:45 +0900

    db_모델링 및 샘플 데이터 INSERT 완료

--- sql.sql
+++ sql.sql
@@ -11,3 +11,9 @@ select * from t_workscope;
 
  select * from t_pj_standard;
  -- 엑셀 '프로젝트 기준' 화면 
+ 
+ select * from t_profile;
+ -- 엑셀 '인원정보' 데이터 
+
+ select * from t_calendar;
+ -- 엑셀 '프로젝트 투입 정보 년,월 별 M/M' 데이터
